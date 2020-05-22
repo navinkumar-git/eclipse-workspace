c478040f8c0d7380687b89d62e301a5070439d80
Navin Kumar <hi.navin@gmail.com>
2020-05-22 07:35:23 +0100

    maven example

---
+++ maven-example/src/main/java/com/example/HelloNavin.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class HelloNavin {
+	public static void main(String[] args) {
+		System.out.println("Hello Navin to Java World");
+	}
+}
---
+++ maven-example/src/test/java/com/example/HelloNavintest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloNavintest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
